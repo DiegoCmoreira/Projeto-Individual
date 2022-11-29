e09f11f6e43847fb62a5bc85c07e0c7cf46793ee
JoaoLimaHeyla <joao.lima@sptech.school>
2022-11-29 14:38:45 -0300

    Nova pagina, oposto, feita.

--- web-data-viz/site/src/database/Voe Alto-script.sql
+++ web-data-viz/site/src/database/Voe Alto-script.sql
@@ -1,17 +1,6 @@
 Create database VoeAlto;
 use VoeAlto;
 
-create table Usuario(
-idUsuario int primary key auto_increment,
-nome varchar(45),
-email varchar(45),
-senha varchar(45),
-fkJogador int,
-foreign key (fkJogador) references Jogador(idJogador),
-fkTime int,
-foreign key (fkTime) references Time(idTime)
-);
-
 create table Jogador(
 idJogador int primary key auto_increment,
 tipo varchar(45),
@@ -19,10 +8,13 @@ altura char(3),
 nivel varchar(45)
 );
 
-create table Time(
-idTime int Primary key auto_increment,
+create table Usuario(
+idUsuario int primary key auto_increment,
 nome varchar(45),
-qtd_jogador char(8)
+email varchar(45),
+senha varchar(45),
+fkJogador int,
+foreign key (fkJogador) references Jogador(idJogador)
 );
 
 create table Historico(
@@ -31,11 +23,8 @@ qtd_vitoria char(8),
 qtd_derrota char(8),
 total_pontos char(8),
 fkUsuario int,
-foreign key (fkUsuario) references Usuario(idUsuario),
-fkTime int,
-foreign key (fkTime) references Time(idTime)
+foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
-
-
- 
+select * from Usuario;
+select * from Jogador;
